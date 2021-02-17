acbf9e313e9ace6e4707542da0ffff41e351a818
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-02-17 11:21:19 +0100

    added test_remo_ds

---
+++ tests/test_remo_ds.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from pyremo import remo_ds as rds
+
+test_data = './data/e056111t2006010100'
+
+
+def test_remo_ds():
+    filename = test_data
+    ds = rds.open_remo_dataset(filename, update_meta=True)
+    assert 'T' in ds
+    assert ds.T.code == 130

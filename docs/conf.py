af1ed65f9491d6b3612cca438a27364c84a3d4cb
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-02-24 01:06:04 +0100

    numpy docs updates

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,24 @@ import pyremo
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
+extensions = [
+        "sphinx.ext.autodoc", 
+        "sphinx.ext.autosummary", 
+        "sphinx.ext.viewcode",
+        "sphinx.ext.extlinks",
+        "sphinx.ext.mathjax",
+        "sphinx.ext.napoleon",
+        "sphinxcontrib.mockautodoc",
+        "numpydoc"
+]
 
+autosummary_generate = True
+
+napoleon_use_param = True
+napoleon_use_rtype = False
+
+numpydoc_class_members_toctree = True
+numpydoc_show_class_members = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

7a83d0e5f252e542cc1176070d9c6a5d5889579f
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-03-04 00:10:47 +0100

    data updates

--- pyremo/data.py
+++ pyremo/data.py
@@ -1,3 +1,13 @@
 
+import os
 
-DRKZ_URL = "https://swift.dkrz.de/v1/dkrz_ffd3ca9004324ad28243244b834f92b1/remo/data"
+
+DKRZ_URL = "https://swift.dkrz.de/v1/dkrz_ffd3ca9004324ad28243244b834f92b1/remo/data"
+
+
+
+
+flake_EUR_11_glcc = os.path.join(DKRZ_URL, 'flake/flake_v3_glcc_defD10.0m_frac_EUR-11.nc')
+flake_EUR_44_glcc = os.path.join(DKRZ_URL, 'flake/flake_v3_glcc_defD10.0m_frac_EUR-44.nc')
+
+

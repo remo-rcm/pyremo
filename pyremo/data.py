0f2c626beaae9964aae9e59fbe2b24ac00015a88
github action <lars.buntemeyer@hzg.de>
2021-06-16 09:07:54 +0200

    updates on the data module for accessing remo surflibs

--- pyremo/data.py
+++ pyremo/data.py
@@ -9,11 +9,11 @@ tutorial_data = os.path.join(DKRZ_URL, 'example/e056111t2006010100.nc')
 
 bodlib_tpl = os.path.join(DKRZ_URL, 'surface-library/lib_{}_frac.nc')
 
-
+from . import remo_ds as rds
 from .remo_ds import open_remo_dataset
+from . import domain as dm
 
-
-def get_file(url):
+def _get_file(url):
     import fsspec
     import xarray as xr
     with fsspec.open(url) as f:
@@ -21,22 +21,52 @@ def get_file(url):
     return ds
 
 
-def surflib(domain='EUR-11'):
+def surflib(domain, crop=True, update_meta=True):
+    """Return a REMO surface library from the DKRZ cloud.
+
+    Please note that by default the surface library will be cropped to
+    the size of the REMO domain. The original libraries contained
+    an original boundary row.
+
+    Requires network connection for online access.
+
+    Parameters
+    ----------
+    domain : str
+        REMO domain short name.
+    crop : bool
+        If True, crop to REMO domain.
+    update_meta : bool
+        If True, update meta information if required.
+
+    Returns
+    -------
+    dataset : xr.Dataset
+        Surface library dataset.
+
+    """
     import fsspec
+    import xarray as xr
     url = bodlib_tpl.format(domain)
+    grid = dm.remo_domain(domain) 
     with fsspec.open(url) as f:
-        ds = open_remo_dataset(f, update_meta=True).squeeze(drop=True)
+        #ds = open_remo_dataset(f, update_meta=True).squeeze(drop=True)
+        ds = xr.open_dataset(f).squeeze(drop=True)
+    if update_meta: ds = rds.update_meta_info(ds)
+    if crop: ds = ds.sel(rlon=grid.rlon, rlat=grid.rlat, method='nearest')
     return ds
 
 
-def example_eur44():
-    import fsspec
-    url = bodlib_tpl.format(domain)
-    with fsspec.open(url) as f:
-        ds = open_remo_dataset(f)
-    return ds
+#def example_eur44():
+#    import fsspec
+#    url = bodlib_tpl.format(domain)
+#    with fsspec.open(url) as f:
+#        ds = open_remo_dataset(f)
+#    return ds
 
 
 def example_output():
+    """Returns a dataset containing REMO example output.
+    """
     from . import remo_ds as rds
     return rds.update_meta_info(get_file(tutorial_data))

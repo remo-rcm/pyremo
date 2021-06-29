028b2609063a8c4da27bdd8ef50419c392ae3ede
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-06-29 22:22:18 +0200

    requirement clean up

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = open("requirements.txt").read().strip().split("\n"), 
+requirements = open("requirements.txt").read().strip().split("\n")
 
 setup_requirements = [
     "pytest-runner",

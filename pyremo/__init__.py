7147a733c2178b095ea6362fca327f45e491114e
github action <lars.buntemeyer@hzg.de>
2021-07-22 12:58:12 +0200

    test bugfix for docs

--- pyremo/__init__.py
+++ pyremo/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.1.0"
 
 from . import core, preproc
 
-from .core.domain import domain_info, remo_domain
+#from .core.domain import domain_info, remo_domain
 from .core.remo_ds import open_remo_dataset, parse_dates, update_meta_info
 from .core import data
 from .tables import domains, codes, vc

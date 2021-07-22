3e0ae44459ac70d0cc2071e1c91527f608a4f987
github action <lars.buntemeyer@hzg.de>
2021-07-22 13:40:00 +0200

    inits updates

--- pyremo/__init__.py
+++ pyremo/__init__.py
@@ -6,7 +6,9 @@ __version__ = "0.1.0"
 
 
 #from . import core, preproc
-#
+from . import preproc
+
+
 from .core.domain import domain_info, remo_domain
 from .core.remo_ds import open_remo_dataset, parse_dates, update_meta_info
 from .core import remo_tutorial as tutorial, remo_ds

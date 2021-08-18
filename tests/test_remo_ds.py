09e31922b80e1c391c2da243d3c376b98a277873
github action <lars.buntemeyer@hzg.de>
2021-08-18 13:35:37 +0200

    test update

--- tests/test_remo_ds.py
+++ tests/test_remo_ds.py
@@ -3,12 +3,9 @@ import pyremo as pr
 
 import pytest
 
-test_data = './tests/data/e056111t2006010100.nc'
-
 
 def test_remo_ds():
-    pytest.importorskip('cdo')
-    filename = test_data
-    ds = pr.open_remo_dataset(filename, update_meta=True)
+    #pytest.importorskip('cdo')
+    ds = pr.tutorial.open_dataset()
     assert 'T' in ds
     assert ds.T.code == 130

d2ac12a1d7fe2013c288220a87c8d0fa10d74aaf
github action <lars.buntemeyer@hzg.de>
2021-11-05 22:01:49 +0100

    added test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+
+
+from pyremo.core import cal
+import datetime as dt
+
+
+
+
+def test_absolute_calendar():
+    calendar = cal.AbsoluteCalendar()
+    assert(dt.datetime(1979,1,1) == calendar.num2date(19790101))
+    assert(dt.datetime(1979,1,1,12) == calendar.num2date(19790101.5))
+    date = dt.datetime(1979,1,1,12)
+    assert(calendar.date2num(date) == 19790101.5)
+    date = dt.datetime(1979,1,1)
+    assert(calendar.date2num(date) == 19790101)
+

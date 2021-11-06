90ada87858b9ae39a1341c26c5617c9f0e25eb44
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-11-06 22:49:34 +0100

    inteface updates

--- pyremo/__init__.py
+++ pyremo/__init__.py
@@ -8,11 +8,12 @@ __email__ = "lars.buntemeyer@hzg.de"
 from . import preproc, physics
 
 from .core.domain import domain_info, remo_domain
-from .core.remo_ds import open_remo_dataset, update_meta_info
+from .core.remo_ds import open_remo_dataset, update_meta_info, preprocess
 from .core.cal import parse_dates, parse_absolute_time
 from .core import remo_tutorial as tutorial, remo_ds, data
 from .tables import domains, vc
 from .core import codes
+from .cmor.cmor import cmorize_variable
 
 
 from .version import version

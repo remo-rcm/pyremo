f8077151cd79f1f879b1de58be059100c3b8b16e
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-11-08 12:27:37 +0100

    __init__ update

--- pyremo/cmor/__init__.py
+++ pyremo/cmor/__init__.py
@@ -1,1 +1,1 @@
-from .remo_cmor import prepare_variable, cmorize_variable, season, season_bounds, mid_of_season
+from .remo_cmor import prepare_variable, cmorize_variable, season, season_bounds, mid_of_season, to_cftime

09cf1045b0f87d2cb4bff1e81a711d59cc669db3
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-11-08 12:35:43 +0100

    cmor test update

--- tests/test_cmor.py
+++ tests/test_cmor.py
@@ -1,21 +1,22 @@
-
-
+import pytest
 import pyremo as pr
 import cftime as cfdt
 import datetime as dt
 
 
+def test_cftime():
+    assert pr.cmor.to_cftime(dt.datetime(2000, 1, 1, 1)) == cfdt.datetime(2000, 1, 1, 1)
 
-def test_season():
-    assert(pr.cmor.to_cftime(dt.datetime(2000,1,1,1)) == cfdt.datetime(2000,1,1,1))
-    assert(pr.cmor.season(dt.datetime(2000,1,1)) == 'DJF')
-    assert(pr.cmor.season(cfdt.datetime(2000,1,1)) == 'DJF')
-    assert(pr.cmor.season(dt.datetime(2001,3,1)) == 'MAM')
-    assert(pr.cmor.season(dt.datetime(2001,6,1)) == 'JJA')
-    assert(pr.cmor.season(dt.datetime(2001,9,1)) == 'SON')
-    assert(pr.cmor.season(dt.datetime(2001,12,1)) == 'DJF')
+
+@pytest.mark.parametrize("dt", [dt, cfdt])
+def test_season(dt):
+    assert pr.cmor.season(dt.datetime(2000, 1, 1)) == "DJF"
+    assert pr.cmor.season(dt.datetime(2001, 3, 1)) == "MAM"
+    assert pr.cmor.season(dt.datetime(2001, 6, 1)) == "JJA"
+    assert pr.cmor.season(dt.datetime(2001, 9, 1)) == "SON"
+    assert pr.cmor.season(dt.datetime(2001, 12, 1)) == "DJF"
     bounds = (dt.datetime(1999, 12, 1, 0, 0), dt.datetime(2000, 3, 1, 0, 0))
-    assert(pr.cmor.season_bounds(dt.datetime(2000,1,1)) == bounds)
-    bounds = (cfdt.datetime(1999, 12, 1, 0, 0), cfdt.datetime(2000, 3, 1, 0, 0))
-    assert(pr.cmor.season_bounds(cfdt.datetime(2000,1,1)) == bounds)
-    assert(pr.cmor.mid_of_season(dt.datetime(2000,1,1)) == dt.datetime(2000, 1, 15, 12, 0))
+    assert pr.cmor.season_bounds(dt.datetime(2000, 1, 1)) == bounds
+    assert pr.cmor.mid_of_season(dt.datetime(2000, 1, 1)) == dt.datetime(
+        2000, 1, 15, 12, 0
+    )

120261e1015a87794123300e4762a7eaf538d6e4
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-11-10 18:43:34 +0100

    cleanup (#24)
    * cleanup
    
    * test updates
    
    * format updates

--- pyremo/cmor/__init__.py
+++ pyremo/cmor/__init__.py
@@ -1,10 +1,5 @@
 from .remo_cmor import (
     prepare_variable,
     cmorize_variable,
-    season,
-    season_bounds,
-    mid_of_season,
     to_cftime,
-    month_bounds,
-    mid_of_month,
 )
--- tests/test_cmor.py
+++ tests/test_cmor.py
@@ -6,35 +6,3 @@ import datetime as dt
 
 def test_cftime():
     assert pr.cmor.to_cftime(dt.datetime(2000, 1, 1, 1)) == cfdt.datetime(2000, 1, 1, 1)
-
-
-@pytest.mark.parametrize("dt", [dt, cfdt])
-def test_season(dt):
-    assert pr.cmor.season(dt.datetime(2000, 1, 1)) == "DJF"
-    assert pr.cmor.season(dt.datetime(2001, 3, 1)) == "MAM"
-    assert pr.cmor.season(dt.datetime(2001, 6, 1)) == "JJA"
-    assert pr.cmor.season(dt.datetime(2001, 9, 1)) == "SON"
-    assert pr.cmor.season(dt.datetime(2001, 12, 1)) == "DJF"
-    bounds = (dt.datetime(1999, 12, 1, 0, 0), dt.datetime(2000, 3, 1, 0, 0))
-    assert pr.cmor.season_bounds(dt.datetime(2000, 1, 1)) == bounds
-    assert pr.cmor.mid_of_season(dt.datetime(2000, 1, 1)) == dt.datetime(
-        2000, 1, 15, 12, 0
-    )
-
-
-def test_month():
-    assert pr.cmor.month_bounds(dt.datetime(2000, 1, 10)) == ( dt.datetime(2000, 1, 1), dt.datetime(2000, 2, 1))
-    assert pr.cmor.month_bounds(dt.datetime(2000, 12, 10)) == ( dt.datetime(2000, 12, 1), dt.datetime(2001, 1, 1))
-    assert pr.cmor.mid_of_month(dt.datetime(2000, 1, 1)) == dt.datetime(2000, 1, 16, 12)
-    # leap years
-    assert pr.cmor.mid_of_month(dt.datetime(2000, 2, 1)) == dt.datetime(2000, 2, 15, 12)
-    assert pr.cmor.mid_of_month(dt.datetime(2001, 2, 1)) == dt.datetime(2001, 2, 15)
-
-
-def test_cfmonth():
-    assert pr.cmor.month_bounds(dt.datetime(2000, 1, 10)) == ( dt.datetime(2000, 1, 1), dt.datetime(2000, 2, 1))
-    # leap years
-    assert pr.cmor.mid_of_month(cfdt.datetime(2000, 2, 1)) == cfdt.datetime(2000, 2, 15, 12)
-    assert pr.cmor.mid_of_month(cfdt.datetime(2001, 2, 1)) == cfdt.datetime(2001, 2, 15)
-    assert pr.cmor.mid_of_month(cfdt.datetime(2000, 2, 1, calendar="360_day")) == cfdt.datetime(2000, 2, 16, calendar="360_day")
-    assert pr.cmor.mid_of_month(cfdt.datetime(2001, 2, 1, calendar="360_day")) == cfdt.datetime(2001, 2, 16, calendar="360_day")

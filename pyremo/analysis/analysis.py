5b51223937f0c86667319b5bab06a01ae01b0460
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-12-05 21:10:22 +0100

    Analysis (#33)
    * added new classes
    
    * added some experimental code
    
    * added obs functions
    
    * added hyras
    
    * moved plot seasons
    
    * added production analyiss notebook
    
    * renamed notebook
    
    * added notebook to docs
    
    * notebook update

--- pyremo/analysis/analysis.py
+++ pyremo/analysis/analysis.py
@@ -1,9 +1,31 @@
 
+import os
+import glob
 
 import numpy as np
 import xarray as xr
 
+from ..archive import archive
+from ..core.remo_ds import preprocess as remo_preprocess
+from ..core import codes
+
 soil_temps = ['TS', 'TSL', 'TEMP2', 'TSN', 'TD3', 'TD4', 'TD5']
+
+
+def open_mfdataset(files, use_cftime=True, parallel=True, data_vars='minimal', chunks={'time':1}, 
+                   coords='minimal', compat='override', drop=None, **kwargs):
+    """optimized function for opening large cf datasets.
+
+    based on https://github.com/pydata/xarray/issues/1385#issuecomment-561920115
+    
+    """
+    def drop_all_coords(ds):
+        return ds.reset_coords(drop=True)
+    ds = xr.open_mfdataset(files, parallel=parallel, decode_times=False, combine='by_coords', 
+                       preprocess=drop_all_coords, decode_cf=False, chunks=chunks,
+                      data_vars=data_vars, coords=coords, compat=compat, **kwargs)
+    return xr.decode_cf(ds, use_cftime=use_cftime)
+
 
 
 class VariableSet():
@@ -53,8 +75,75 @@ def seasonal_mean(da):
     return da.groupby('time.season').mean(dim='time')
 
 
-
-class Experiment():
+class RemoExperiment():
     
-    def __init__(path):
-        return None
+    def __init__(self, path, time_range=None, preprocess=True):
+        self.archive = archive.RemoArchive(path, force_parse=True)
+        self.first_tfile = self.archive.first_tfile()
+        
+        self.ds = self._init_dataset(preprocess, time_range)
+        
+    def _init_dataset(self, preprocess=True, time_range=None):
+        monthly = open_mfdataset(self.archive.monthly(time_range=time_range))
+        if preprocess is True:
+            monthly = remo_preprocess(monthly)
+        lsm = xr.open_dataset(self.first_tfile).BLA.squeeze(drop=True)
+        orog = xr.open_dataset(self.first_tfile).FIB.squeeze(drop=True)
+        ds = xr.merge([monthly, lsm, orog])
+        ds["mask"] = xr.where(lsm > 0, 1, 0)
+        return ds
+
+    @property       
+    def mask(self):
+        return self.ds.mask
+        
+    @property
+    def variables(self):
+        return list(self.monthly.data_vars)
+
+    def dataset(self, variables=None):
+        if variables is None:
+            ds = self.ds
+        else:
+            if not isinstance(variables, list):
+                variables = [variables]
+            names = [codes.get_dict(id)['variable'] for id in variables]
+            ds = xr.merge([self.ds[v] for v in names])
+        ds.attrs = self.monthly.attrs
+        return ds
+    
+    def soil(self):
+        return stack_variables(self.ds, soil_temps, name="soil_temperatures", long_name="soil temperatures")
+    
+    def spin_up(self):
+        return weighted_field_mean(self.soil())
+    
+    
+def get_regridder(finer, coarser, method='bilinear', **kwargs):
+    """
+    Function to regrid data bilinearly to a coarser grid
+    """
+    
+    import xesmf as xe
+    
+    return xe.Regridder(finer, coarser, method=method, **kwargs)
+
+
+def compare_seasons(ds1, ds2, orog1=None, orog2=None, do_height_correction=False,
+                   regrid='ds1'):
+    ds1 = ds1.copy()
+    ds2 = ds2.copy()
+    ds1_seasmean = seasonal_mean(ds1)
+    ds2_seasmean = seasonal_mean(ds2)
+    if regrid == "ds1":
+        regridder = get_regridder(ds1, ds2)
+        print(regridder)
+        ds1_seasmean = regridder(ds1_seasmean)
+        
+    if do_height_correction is True:
+        orog1 = regridder(orog1)
+        ds1_seasmean += height_correction(orog1, orog2)
+    return ds2_seasmean - ds1_seasmean    
+    return xr.where(ds1_seasmean.mask, ds2_seasmean - ds1_seasmean, np.nan)
+
+
--- pyremo/archive/archive.py
+++ pyremo/archive/archive.py
@@ -1,5 +1,27 @@
 
+import os
 
+import pandas as pd
+from pathlib import Path
+
+from warnings import warn
+
+import tarfile
+
+import parse
+
+file_pattern = "e{usr_nr:3d}{exp_nr:3d}{type:1}{date}"
+efile_pattern = "e{usr_nr:3d}{exp_nr:3d}{type:1}_c{code:3d}_{date}"
+
+date_patterns = ["%Y", "%Y%m", "%Y%m%d", "%Y%m%d", "%Y%m%d%H", "%Y%m%d%H%M"]
+
+patterns = [efile_pattern, file_pattern]
+
+try:
+    from tqdm import tqdm
+except:
+    def tqdm(x):
+        return x
 
 def cdo_call(options='', op='', input='', output=None):
     call = "cdo {} {} {} {}".format(options, op, input, output)
@@ -41,25 +63,149 @@ def get_filename_from_archive(tar, pattern):
 def extract_file(filename, pattern, path):
     open_tar = tarfile.open(filename, 'r')
     filename = get_filename_from_archive(open_tar, pattern)
-    print(filename)
     #dest = os.path.join(scratch, filename)
     open_tar.extract(filename, path=path)
     return os.path.join(path, filename)
 
 
-def extract_files(tars, pattern="", path=None, parallel=False):
+def extract_files(tars, pattern="", path=None, delayed=False, compute=False):
+    if not isinstance(tars, list):
+        tars = [tars]
     if path is None:
         path = os.path.join(os.environ['SCRATCH'], '_archive_extract')
-    else:
-        path = ""
     filenames = []
-    if parallel is True:
-        from dask import delayed
-        futures = []
+    if delayed is True:
+        from dask import delayed as delay
     else:
-        def delayed(x):
+        def delay(x):
             return x
         futures = None
     for tar in tars:
-        filenames.append(delayed(extract_file)(tar, pattern, path))
+        filenames.append(delay(extract_file)(tar, pattern, path))
+    if delayed is True and compute is True:
+        import dask
+        return list(dask.compute(*filenames))
     return filenames
+
+
+def _find_files(dir, file_template="e*", exclude=None):
+    """search a directory for remo output."""
+    pattern = file_template
+    return [path.absolute() for path in Path(dir).rglob(pattern)]
+
+
+def _parse_file(file):
+    file = Path(file).stem
+    for pattern in patterns:
+        finfo = parse.parse(pattern, file)
+        if finfo:
+            return finfo.named
+
+
+def _parse_filepathes(filepathes, parse_dates=True):
+    finfos = []
+    for f in tqdm(filepathes):
+        path = Path(f)
+        finfo = _parse_file(path.stem)
+        if finfo is None:
+            warn("could not parse: ", path.stem)
+            continue
+        finfo["path"] = str(path)
+        finfo["suffix"] = path.suffix
+        if parse_dates is True:
+            for dpat in date_patterns:
+                try:
+                    finfo["date"] = pd.to_datetime(finfo["date"], format=dpat)
+                    break
+                except:
+                    pass
+        finfos.append(finfo)
+    return finfos
+        
+        
+def get_data_catalog(dir, parse_dates=False, exclude=None):
+    filepathes = _find_files(dir, exclude=exclude)
+    df = pd.DataFrame(_parse_filepathes(filepathes, parse_dates=parse_dates))
+    try:
+        df["code"] = df.code.astype(pd.Int64Dtype())
+    except:
+        pass
+    return df
+
+
+def _time_range(df, time_range):
+    return df[(df['date'] >= time_range[0]) & (df['date'] <= time_range[1])]
+
+
+def _search(df, time_range=None, **kwargs):
+    for key, value in kwargs.items():
+        df = df[df[key] == value]
+    if time_range is not None:
+            return _time_range(df, time_range)
+    return df
+
+
+class RemoCatalog():
+    
+    def __init__(self, path, force_parse=False):
+        self.path = path
+        self.csv = os.path.join(self.path, 'data_catalog.csv')
+        if os.path.isfile(self.csv) and force_parse is False:
+            self.df = pd.read_csv(self.csv)
+        else:
+            self.df = self.parse(path, parse_dates=True)
+            self.to_csv()
+            
+    def __add__(self, other):
+        df = pd.concat([self.df, other.df])
+        cat = RemoCatalog(self.path)
+        cat.df = df
+        return cat
+    
+    def parse(self, path=None, parse_dates=True):
+        if path is None:
+            path = self.path
+        return get_data_catalog(path, parse_dates=parse_dates)
+        
+    def to_csv(self, path=None):
+        self.df.to_csv(self.csv, index=False)
+  
+    def search(self, time_range=None, **kwargs):
+        return _search(self.df, time_range=time_range, **kwargs).sort_values('date') 
+
+    def archive(self, type, suffix='.tar', time_range=None, **kwargs):
+        return _search(self.df, type=type, suffix=suffix,
+                       time_range=time_range, **kwargs).sort_values('date')
+    
+    def update(self, filepathes):
+        self.df = pd.concat([self.df, pd.DataFrame(_parse_filepathes(filepathes))])
+    
+    
+class RemoArchive():
+    
+    def __init__(self, path, extract_path=None, force_parse=False):
+        self.path = path
+        if extract_path is None:
+            self.extract_path = os.path.join(os.environ['SCRATCH'], '_archive_extract')
+        else:
+            self.extract_path = extract_path
+        self.catalog = RemoCatalog(self.path, force_parse) + RemoCatalog(self.extract_path, force_parse=True)
+        #self.df = get_data_catalog(self.extract_path, parse_dates=True)
+        
+    def extract(self, type, code=None, time=None):
+        pass
+                 
+    def _extract_code(self, code, time_range=None, parallel=False, **kwargs):
+        pattern = "c{:03d}".format(code)
+        tars = list(self.catalog.archive('e', time_range=time_range, **kwargs).path)
+        filepathes = extract_files(tars, pattern=pattern, path=self.extract_path, 
+                             delayed=parallel, compute=parallel)
+        self.catalog.update(filepathes)
+        return filepathes
+    
+    def first_tfile(self):
+        tar = list(self.catalog.archive('t').path)[0]
+        return extract_files(tar, pattern="010106")[0]
+    
+    def monthly(self, time_range=None):
+        return list(self.catalog.search(type='m', suffix='.nc', time_range=time_range).path)        

614b2b4cf0ef9a791a0a6c7c4a0aefef1edabbc7
Ludwig Lierhammer <ludwig.lierhammer@hereon.de>
2022-02-03 17:32:58 +0100

    Seasonal means (#39)
    * Comment on compare_seasons
    
    * Optimized function to calculate seasonal means
    
    Co-authored-by: Ludwig Lierhammer <ludwig.lierhammer@hzg.de>

--- pyremo/analysis/analysis.py
+++ pyremo/analysis/analysis.py
@@ -73,12 +73,30 @@ def daily_sum(da):
     
     return da.groupby('time.day').sum(dim='time')
 
-def seasonal_mean(da):
+def monthly_sum(da):
     """
-    Function to compute seasonal means with a simple groupby approach
+    Function to compute dailymonthly means with a simple groupby approach
     """
     
-    return da.groupby('time.season').mean(dim='time')
+    return da.groupby('time.month').mean(dim='time')
+
+def seasonal_mean(da):
+    """
+    Optimized function to calculate seasonal averages from time series of monthly means
+
+    based on: https://xarray.pydata.org/en/stable/examples/monthly-means.html
+    """
+
+    #Get number od days for each month
+    month_length = da.time.dt.days_in_month
+    # Calculate the weights by grouping by 'time.season'.
+    weights = (month_length.groupby("time.season") / month_length.groupby("time.season").sum())
+
+    # Test that the sum of the weights for each season is 1.0
+    np.testing.assert_allclose(weights.groupby("time.season").sum().values, np.ones(4))
+
+    # Calculate the weighted average
+    return (da * weights).groupby("time.season").sum(dim="time")
 
 
 class RemoExperiment():
@@ -135,8 +153,40 @@ def get_regridder(finer, coarser, method='bilinear', **kwargs):
     return xe.Regridder(finer, coarser, method=method, **kwargs)
 
 
-def compare_seasons(ds1, ds2, orog1=None, orog2=None, do_height_correction=False,
-                   regrid='ds1'):
+def compare_seasons(ds1, ds2, regrid='ds1', do_height_correction=False, orog1=None, orog2=None):
+    """
+    Function to compare seasonal means of two datasets.
+
+    Paramters
+    ---------
+    ds1 : xarray.Dataset
+        First variable data for comparision. Temporal resolution has to be less than monthly.
+        ds1 is mainly model output data.
+        ds2 is subtracted from ds1.
+    ds2 : xarray.Dataset
+        Second variable data for comparision. Temporal resolution has to be less than monthly.
+        ds2 is mainly observational or reanalysis data.
+        ds2 is subtracted from ds1. 
+    regrid : {"ds1", "ds2"}, optional
+        Denotes the dataset to be bilinearly regridded. Specify the dataset with the finer spatial resolution:
+
+        - "ds1": Regrid ds1 to ds2's grid with coarser spatial resolution.
+        - "ds2": Regrid ds2 to ds1's grid with coarser spatial resolution.
+    do_height_correction : bool, optional
+        If ``do_height_correction=True``, do a height correction on ds1 using two orography files orog1 and orog2.
+    orog1 : xarray.Dataset, optional
+        Use only if ``do_height_correction=True``. 
+        Specify a orography file referring to ds1.
+    orog2 : xarray.Dataset, optional
+        Use only if ``do_height_correction=True``. 
+        Specify a orography file referring to ds2.
+
+    Returns
+    -------
+    seasonal_comparision : xarray.Dataset
+        Spatial mean differences of two datasets
+
+    """
     ds1 = ds1.copy()
     ds2 = ds2.copy()
     ds1_seasmean = seasonal_mean(ds1)

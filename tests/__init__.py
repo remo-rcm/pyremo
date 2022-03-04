080b882f8d1ff6637c65965bb898a18cabdd6b3a
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2022-03-04 22:32:41 +0100

    added test of pressure interpolation (#43)
    * function cleaning
    
    * added import test
    
    * update of whats new
    
    * added prsint test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,18 @@
-"""Unit test package for pyremo."""
+"""Unit test package for cordex."""
+
+import importlib
+
+import pytest
+
+
+def _importorskip(modname):
+    try:
+        importlib.import_module(modname)
+        has = True
+    except ImportError:  # pragma: no cover
+        has = False
+    func = pytest.mark.skipif(not has, reason=f"requires {modname}")
+    return has, func
+
+
+has_pydruint, requires_pydruint = _importorskip("pydruint")

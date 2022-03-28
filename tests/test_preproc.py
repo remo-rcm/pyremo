f0840a7f5f3a62a4ed100b03588c9276f98b5c48
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2022-03-28 16:28:56 +0200

    format updates

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -1,9 +1,9 @@
-
 import pytest
 import pyremo as pr
 from pyremo.preproc.double_nesting import process_file
 
 from . import requires_pyintorg
+
 
 @pytest.fixture
 def ds():
@@ -12,11 +12,11 @@ def ds():
 
 @requires_pyintorg
 def test_double_nesting(ds):
-    em = pr.domain_info('EUR-44')
-    hm = pr.domain_info('EUR-11')
-    hm_ds = pr.remo_domain('EUR-11')
-    em_ds = pr.remo_domain('EUR-44')
-    vc = pr.vc.tables['vc_27lev']
-    surflib = pr.data.surflib('EUR-11', crop=False)
+    em = pr.domain_info("EUR-44")
+    hm = pr.domain_info("EUR-11")
+    hm_ds = pr.remo_domain("EUR-11")
+    em_ds = pr.remo_domain("EUR-44")
+    vc = pr.vc.tables["vc_27lev"]
+    surflib = pr.data.surflib("EUR-11", crop=False)
     ads = process_file(ds, em, hm, vc, surflib)
     print(ads)

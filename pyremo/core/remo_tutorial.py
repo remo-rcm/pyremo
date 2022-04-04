00cf4d7c7fbb392562b1c0735e1dddd0e60ccb49
github action <lars.buntemeyer@hzg.de>
2022-04-04 15:20:31 +0200

    updated tutorials and data source

--- pyremo/core/remo_tutorial.py
+++ pyremo/core/remo_tutorial.py
@@ -6,8 +6,9 @@ Useful for:
 
 """
 ### code stolen from xarray, I am sorry!
-import hashlib
-import os as _os
+import os
+import pathlib
+
 from urllib.request import urlretrieve
 
 import numpy as np
@@ -18,7 +19,34 @@ from xarray import Dataset
 
 from . import data
 
-_default_cache_dir = _os.sep.join(("~", ".pyremo_tutorial_data"))
+
+base_url = "https://github.com/remo-rcm/pyremo-data"
+version = "main"
+
+#_default_cache_dir = _os.sep.join(("~", ".pyremo_tutorial_data"))
+_default_cache_dir_name = "pyremo_tutorial_data"
+
+
+external_urls = {}  # type: dict
+
+file_formats = {
+    "remo_EUR-44": 3,
+    "rasm": 3,
+    "ROMS_example": 4,
+    "tiny": 3,
+    "eraint_uvz": 3,
+}
+
+
+def _construct_cache_dir(path):
+    import pooch
+
+    if isinstance(path, os.PathLike):
+        path = os.fspath(path)
+    elif path is None:
+        path = pooch.os_cache(_default_cache_dir_name)
+
+    return path
 
 
 def file_md5_checksum(fname):
@@ -28,79 +56,105 @@ def file_md5_checksum(fname):
     return hash_md5.hexdigest()
 
 
+def _check_netcdf_engine_installed(name):
+    version = file_formats.get(name)
+    if version == 3:
+        try:
+            import scipy  # noqa
+        except ImportError:
+            try:
+                import netCDF4  # noqa
+            except ImportError:
+                raise ImportError(
+                    f"opening tutorial dataset {name} requires either scipy or "
+                    "netCDF4 to be installed."
+                )
+    if version == 4:
+        try:
+            import h5netcdf  # noqa
+        except ImportError:
+            try:
+                import netCDF4  # noqa
+            except ImportError:
+                raise ImportError(
+                    f"opening tutorial dataset {name} requires either h5netcdf "
+                    "or netCDF4 to be installed."
+                )
+              
+
 # idea borrowed from Seaborn
 def open_dataset(
-    name="remo_output_3d",
+    name="remo_EUR-44",
     cache=True,
-    cache_dir=_default_cache_dir,
-    data_url=data.DKRZ_URL,
-    folder="tutorial",
+    cache_dir=None,
+    *,
+    engine=None,
     **kws,
 ):
     """
     Open a dataset from the online repository (requires internet).
-
     If a local copy is found then always use that to avoid network traffic.
-
+    Available datasets:
+    * ``"remo_EUR-44"``: Example 3D remo output on EUR-44 domain.
+    * ``"rasm"``: Output of the Regional Arctic System Model (RASM)
+    * ``"ROMS_example"``: Regional Ocean Model System (ROMS) output
+    * ``"tiny"``: small synthetic dataset with a 1D data variable
+    * ``"era5-2mt-2019-03-uk.grib"``: ERA5 temperature data over the UK
+    * ``"eraint_uvz"``: data from ERA-Interim reanalysis, monthly averages of upper level data
     Parameters
     ----------
     name : str
-        Name of the file containing the dataset. If no suffix is given, assumed
-        to be netCDF ('.nc' is appended)
+        Name of the file containing the dataset.
         e.g. 'air_temperature'
-    cache_dir : str, optional
+    cache_dir : path-like, optional
         The directory in which to search for and write cached data.
     cache : bool, optional
         If True, then cache data locally for use on subsequent calls
-    data_url : str
-        url where the data is stored
-    folder : str
-        folder where the data is stored
-    kws : dict, optional
+    **kws : dict, optional
         Passed to xarray.open_dataset
-
     See Also
     --------
     xarray.open_dataset
-
     """
-    root, ext = _os.path.splitext(name)
-    if not ext:
-        ext = ".nc"
-    fullname = root + ext
-    longdir = _os.path.expanduser(cache_dir)
-    localfile = _os.sep.join((longdir, fullname))
-    md5name = fullname + ".md5"
-    md5file = _os.sep.join((longdir, md5name))
+    try:
+        import pooch
+    except ImportError as e:
+        raise ImportError(
+            "tutorial.open_dataset depends on pooch to download and manage datasets."
+            " To proceed please install pooch."
+        ) from e
 
-    if not _os.path.exists(localfile):
+    logger = pooch.get_logger()
+    logger.setLevel("WARNING")
 
-        # This will always leave this directory on disk.
-        # May want to add an option to remove it.
-        if not _os.path.isdir(longdir):
-            _os.mkdir(longdir)
+    cache_dir = _construct_cache_dir(cache_dir)
+    if name in external_urls:
+        url = external_urls[name]
+    else:
+        path = pathlib.Path(name)
+        if not path.suffix:
+            # process the name
+            default_extension = ".nc"
+            if engine is None:
+                _check_netcdf_engine_installed(name)
+            path = path.with_suffix(default_extension)
+        elif path.suffix == ".grib":
+            if engine is None:
+                engine = "cfgrib"
 
-        url = "/".join((data_url, folder, fullname))
-        urlretrieve(url, localfile)
-        url = "/".join((data_url, folder, md5name))
-        urlretrieve(url, md5file)
-        localmd5 = file_md5_checksum(localfile)
-        with open(md5file) as f:
-            remotemd5 = f.read()
-        if localmd5 != remotemd5:
-            _os.remove(localfile)
-            msg = """
-            MD5 checksum does not match, try downloading dataset again.
-            """
-            raise OSError(msg)
+        url = f"{base_url}/raw/{version}/{path.name}"
 
-    ds = _open_dataset(localfile, **kws)
-
+    # retrieve the file
+    filepath = pooch.retrieve(url=url, known_hash=None, path=cache_dir)
+    ds = _open_dataset(filepath, engine=engine, **kws)
     if not cache:
         ds = ds.load()
-        _os.remove(localfile)
+        pathlib.Path(filepath).unlink()
 
     return ds
+
+
+
 
 
 def load_dataset(*args, **kwargs):
@@ -116,28 +170,6 @@ def load_dataset(*args, **kwargs):
         return ds.load()
 
 
-def scatter_example_dataset():
-    A = DataArray(
-        np.zeros([3, 11, 4, 4]),
-        dims=["x", "y", "z", "w"],
-        coords=[
-            np.arange(3),
-            np.linspace(0, 1, 11),
-            np.arange(4),
-            0.1 * np.random.randn(4),
-        ],
-    )
-    B = 0.1 * A.x ** 2 + A.y ** 2.5 + 0.1 * A.z * A.w
-    A = -0.1 * A.x + A.y / (5 + A.z) + A.w
-    ds = Dataset({"A": A, "B": B})
-    ds["w"] = ["one", "two", "three", "five"]
-
-    ds.x.attrs["units"] = "xunits"
-    ds.y.attrs["units"] = "yunits"
-    ds.z.attrs["units"] = "zunits"
-    ds.w.attrs["units"] = "wunits"
-
-    ds.A.attrs["units"] = "Aunits"
-    ds.B.attrs["units"] = "Bunits"
-
-    return ds
+def mpi_esm():
+    pass
+    

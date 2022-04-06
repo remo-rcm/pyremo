aadb5f2b483e45fdc4512939567082be62bc0d75
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2022-04-06 15:46:30 +0200

    added tests for cmorizing time series

--- tests/test_cmor.py
+++ tests/test_cmor.py
@@ -11,14 +11,28 @@ def test_cftime():
     assert prcmor.to_cftime(dt.datetime(2000, 1, 1, 1)) == cfdt.datetime(2000, 1, 1, 1)
 
 
-def test_cmorizer():
-    surflib = pr.data.surflib("EUR-11")
+def test_cmorizer_fx():
+    ds = pr.data.surflib("EUR-11")
     eur11 = cx.cordex_domain("EUR-11")
-    surflib = surflib.assign_coords({"lon": eur11.lon, "lat": eur11.lat})
+    ds = ds.assign_coords({"lon": eur11.lon, "lat": eur11.lat})
     filename = prcmor.cmorize_variable(
-        surflib,
+        ds,
         "orog",
         cmor_table=cmip6_cmor_table("CMIP6_fx"),
+        dataset_table=cordex_cmor_table("CORDEX_remo_example"),
+        CORDEX_domain="EUR-11",
+        time_units=None,
+        allow_units_convert=True,
+    )
+    
+def test_cmorizer_mon():
+    ds = pr.tutorial.open_dataset("remo_EUR-11_TEMP2")
+    eur11 = cx.cordex_domain("EUR-11")
+    ds = ds.assign_coords({"lon": eur11.lon, "lat": eur11.lat})
+    filename = prcmor.cmorize_variable(
+        ds,
+        "tas",
+        cmor_table=cmip6_cmor_table("CMIP6_Amon"),
         dataset_table=cordex_cmor_table("CORDEX_remo_example"),
         CORDEX_domain="EUR-11",
         time_units=None,

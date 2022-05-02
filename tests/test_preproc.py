c26cef80a2c58c20530013414bf3b12baa4b0f74
github action <lars.buntemeyer@hzg.de>
2022-05-03 00:26:58 +0200

    added gcm preproc test

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -1,7 +1,7 @@
 import pytest
 
 import pyremo as pr
-from pyremo.preproc import gfile
+from pyremo.preproc import gfile, remap
 from pyremo.tutorial import mpi_esm, mpi_esm_tos
 
 from . import requires_pyintorg
@@ -30,5 +30,8 @@ def test_double_nesting(ds):
 
 @requires_pyintorg
 def test_cf_preproc(gcm_data):
-    print(gcm_data)
-    pass
+    domain_info = pr.domain_info("EUR-11")
+    surflib = pr.data.surflib("EUR-11", crop=False)
+    vc = pr.vc.tables["vc_27lev"]
+    ads = remap(gcm_data, domain_info, vc, surflib)
+    assert "T" in ads

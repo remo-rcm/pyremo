0ab457d7ba6f828497059797f66d218d26ca954a
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2022-06-27 17:46:13 +0200

    fixed version bug

--- pyremo/__init__.py
+++ pyremo/__init__.py
@@ -8,7 +8,7 @@ from .core.remo_ds import open_remo_dataset, preprocess, update_meta_info
 from .tables import domains, vc
 
 try:
-    __version__ = pkg_resources.get_distribution("py-cordex").version
+    __version__ = pkg_resources.get_distribution("pyremo").version
 except Exception:
     # Local copy or not installed with setuptools.
     # Disable minimum version checks on downstream libraries.

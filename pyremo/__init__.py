3310201d8b938a7e3a3084ce40004b86359691ea
github action <lars.buntemeyer@gmail.com>
2022-07-21 14:06:07 +0200

    added magic numbers API

--- pyremo/__init__.py
+++ pyremo/__init__.py
@@ -3,7 +3,7 @@ import pkg_resources
 from . import physics, tutorial
 from .core import codes, data, remo_ds
 from .core.cal import parse_absolute_time, parse_dates
-from .core.domain import domain_info, remo_domain
+from .core.domain import domain_info, magic_number, magic_numbers, remo_domain
 from .core.remo_ds import open_remo_dataset, preprocess, update_meta_info
 from .tables import domains, vc
 
@@ -26,6 +26,8 @@ __all__ = [
     "remo_domain",
     "open_remo_dataset",
     "preprocess",
+    "magic_number",
+    "magic_numbers",
     "update_meta_info",
     "domains",
     "vc",

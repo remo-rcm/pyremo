40c1a106344341a8b7b9a278d0221312aa3b3f9a
github action <lars.buntemeyer@gmail.com>
2022-08-04 11:02:58 +0200

    fixed docs dependecnies

--- pyremo/preproc/__init__.py
+++ pyremo/preproc/__init__.py
@@ -1,7 +1,5 @@
 from ._remap import remap, remap_remo, to_netcdf, to_tar
-from .cf import get_gfile  # , to_cfdatetime
-
-# from .cf import get_gfile, to_cfdatetime
+from .cf import get_gfile, to_cfdatetime
 from .core import gfile
 from .era5 import ERA5
 from .utils import write_forcing_file
@@ -14,6 +12,6 @@ __all__ = [
     "gfile",
     "ERA5",
     "write_forcing_file",
-    "get_gfile"
-    #    "to_cfdatetime",
+    "get_gfile",
+    "to_cfdatetime",
 ]

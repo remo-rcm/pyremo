983287620469f62bd323ace5055e6dfe00ab3f97
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2023-07-18 17:29:35 +0200

    swich docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -121,8 +121,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_rtd_theme"
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
+# html_theme = "furo"
 # html_theme = "sphinx_book_theme"
 html_title = ""
 

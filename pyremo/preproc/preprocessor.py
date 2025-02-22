b4bc63c900e7ee2c778ac1be4861182833297bfc
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2025-02-22 21:20:38 +0100

    added preprocessor module

---
+++ pyremo/preproc/preprocessor.py
@@ -0,0 +1,542 @@
+import os
+import tempfile
+from os import path as op
+
+import dask
+import xarray as xr
+from pyremo.preproc import get_gcm_dataset, get_gcm_gfile, remap
+
+import pyremo as pr
+from ..remo_ds import update_meta_info
+from .era5 import compute_gfile
+from .utils import datelist, ensure_dir
+
+fillvars = ["TSW", "SEAICE", "TSI"]
+vcs = [
+    "hyai",
+    "hybi",
+    "hyam",
+    "hybm",
+    "akgm",
+    "bkgm",
+    "ak",
+    "bk",
+    "rotated_latitude_longitude",
+]
+static = vcs + ["rotated_latitude_longitude"]
+
+
+dkrz_template = {
+    "path_template": "/pool/data/ERA5/{era_id}/{level_type}/{dataType}/{frequency}/{code:03d}",
+    "file_template": "{era_id}{level_type}{typeid}_{frequency}_{date}_{code:03d}.grb",
+}
+
+cloud_urls = {
+    "gc": "https://storage.googleapis.com/cmip6/pangeo-cmip6.json",
+    "aws": "https://cmip6-pds.s3.amazonaws.com/pangeo-cmip6.json",
+}
+
+
+def open_zstore(zstore):
+    """
+    Open a Zarr store.
+
+    Parameters
+    ----------
+    zstore : str
+        The path to the Zarr store.
+
+    Returns
+    -------
+    xarray.Dataset
+        The opened dataset.
+    """
+    import fsspec
+
+    # logger.debug(f"opening: {zstore}")
+    return xr.open_zarr(store=fsspec.get_mapper(zstore, anon=True), consolidated=True)
+
+
+def get_dsets(df):
+    """
+    Get datasets from a DataFrame.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame containing the dataset information.
+
+    Returns
+    -------
+    dict
+        Dictionary of datasets.
+    """
+    dsets = {row.variable_id: open_zstore(row.zstore) for row in df.itertuples()}
+    return dsets
+
+
+def merge_dsets(dsets, ref="ta"):
+    """
+    Merge datasets with a reference dataset.
+
+    Parameters
+    ----------
+    dsets : dict
+        Dictionary of datasets to merge.
+    ref : str, optional
+        Reference variable ID, by default "ta".
+
+    Returns
+    -------
+    list
+        List of merged datasets.
+    """
+    ref_ds = dsets[ref]
+    dsets_list = [ref_ds.convert_calendar(ref_ds.time.dt.calendar, use_cftime=True)]
+    for variable_id, ds in dsets.items():
+        if "time" in ds.coords:
+            ds = ds.convert_calendar(ds.time.dt.calendar, use_cftime=True)
+        if variable_id != ref:
+            dsets_list.append(ds[variable_id])
+    return dsets_list
+
+
+def get_pangeo_catalog(
+    url,
+    source_id,
+    experiment_id="historical",
+    member_id="r1i1p1f1",
+    activity_id=None,
+    **kwargs,
+):
+    """
+    Get Zarr datasets from the cloud.
+
+    Parameters
+    ----------
+    url : str
+        URL of the cloud storage.
+    source_id : str
+        Source ID of the dataset.
+    experiment_id : str, optional
+        Experiment ID, by default "historical".
+    member_id : str, optional
+        Member ID, by default "r1i1p1f1".
+    activity_id : str, optional
+        Activity ID, by default None.
+
+    Returns
+    -------
+    intake.catalog
+        Intake catalog of the searched datasets.
+    """
+    import intake
+
+    catalog = intake.open_esm_datastore(url)
+    if activity_id is None:
+        activity_id = "CMIP" if experiment_id == "historical" else "ScenarioMIP"
+
+    return catalog.search(
+        activity_id=activity_id,
+        experiment_id=experiment_id,
+        table_id=["6hrLev", "fx", "Oday"],
+        variable_id=["ta", "ua", "va", "hus", "sftlf", "orog", "tos"],
+        member_id=member_id,
+        source_id=source_id,
+        **kwargs,
+    )
+
+
+def get_filename(date, expid="000000", template=None):
+    """
+    Get the filename for a given date and experiment ID.
+
+    Parameters
+    ----------
+    date : datetime-like
+        The date for the filename.
+    expid : str, optional
+        Experiment ID, by default "000000".
+    template : str, optional
+        Filename template, by default None.
+
+    Returns
+    -------
+    str
+        Formatted filename.
+    """
+    if template is None:
+        template = "a{expid}a{date:%Y%m%d%H}.nc"
+    return template.format(expid=expid, date=date)
+
+
+def get_outpath(path, time):
+    """
+    Get the output path for a given time.
+
+    Parameters
+    ----------
+    path : str
+        Path template.
+    time : datetime-like
+        Time for the output path.
+
+    Returns
+    -------
+    str
+        Formatted output path.
+    """
+    outpath = path.format(date=time)
+    if not os.path.isdir(outpath):
+        os.makedirs(outpath)
+    return outpath
+
+
+def write_forcing_file(
+    ds, path=None, expid="000000", template=None, missval=1.0e20, **kwargs
+):
+    """
+    Write a forcing file.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset to write.
+    path : str, optional
+        Output path, by default None.
+    expid : str, optional
+        Experiment ID, by default "000000".
+    template : str, optional
+        Filename template, by default None.
+    missval : float, optional
+        Missing value, by default 1.0e20.
+
+    Returns
+    -------
+    str
+        Path to the written file.
+    """
+    if path is None:
+        path = "./"
+    fname = op.join(path, get_filename(ds.time.item(), expid, template))
+    for v in ds.data_vars:
+        var = ds[v]
+        if v in fillvars:
+            var.encoding = {"_FillValue": missval}
+        else:
+            var.encoding = {"_FillValue": None}
+        # expand time dim is necessary for REMO input forcing
+        if v not in static and "time" not in var.dims:
+            ds[v] = var.expand_dims("time")
+    ds.to_netcdf(fname, **kwargs)
+    ds.close()
+    return fname
+
+
+def prepare_surflib(surflib):
+    """
+    Prepare the surface library.
+
+    Parameters
+    ----------
+    surflib : str
+        Path to the surface library.
+
+    Returns
+    -------
+    xarray.Dataset
+        Prepared surface library dataset.
+    """
+    return update_meta_info(xr.open_dataset(surflib).load()).squeeze(drop=True)
+
+
+class Preprocessor:
+    """
+    Preprocessor class for preparing input data.
+
+    Parameters
+    ----------
+    expid : str
+        Experiment ID.
+    domain : dict or str
+        Domain information.
+    vc : str or dict
+        Vertical coordinate information.
+    surflib : str
+        Path to the surface library.
+    outpath : str, optional
+        Output path, by default None.
+    scratch : str, optional
+        Scratch directory, by default None.
+    """
+
+    def __init__(
+        self, expid, surflib, domain=None, vc="vc_49lev", outpath=None, scratch=None
+    ):
+        if scratch is None:
+            scratch = os.environ["SCRATCH"]
+        self.scratch = tempfile.TemporaryDirectory(dir=scratch)
+        # logger.debug(f"scratch: {self.scratch.name}")
+        if isinstance(vc, str):
+            self.vc = pr.vc.tables[vc]
+        else:
+            self.vc = vc
+        self.expid = expid
+        self.surflib = prepare_surflib(surflib)
+        self.outpath = outpath
+        self._init_domain_info(domain)
+
+    def _clean(self):
+        """
+        Clean up the scratch directory.
+        """
+        # logger.debug(f"cleaning up: {self.scratch.name}")
+        self.scratch.cleanup()
+
+    def _init_domain_info(self, domain=None):
+        if isinstance(domain, dict):
+            self.domain_info = domain
+        elif isinstance(domain, str):
+            self.domain_info = pr.domain_info(domain)
+        else:
+            domain_info = self.surflib.cx.info()
+            domain_info["nlon"] -= 2
+            domain_info["nlat"] -= 2
+            domain_info["ll_lon"] += domain_info["dlon"]
+            domain_info["ll_lat"] += domain_info["dlat"]
+            self.domain_info = domain_info
+
+    def write(self, ds, outpath):
+        """
+        Write the dataset to a forcing file.
+
+        Parameters
+        ----------
+        ds : xarray.Dataset
+            Dataset to write.
+        outpath : str
+            Output path.
+
+        Returns
+        -------
+        str
+            Path to the written file.
+        """
+        return write_forcing_file(ds, path=outpath, expid=self.expid)
+
+    @dask.delayed
+    def preprocess(self, date, outpath=None):
+        """
+        Preprocess the dataset for a given date.
+
+        Parameters
+        ----------
+        date : datetime-like
+            Date for preprocessing.
+        outpath : str, optional
+            Output path, by default None.
+
+        Returns
+        -------
+        xarray.Dataset or str
+            Preprocessed dataset or path to the written file.
+        """
+        ds = self.get_input_dataset(date)
+        ads = remap(ds, self.domain_info, self.vc, self.surflib)
+        if outpath is None:
+            return ads
+        return self.write(ads, outpath)
+
+    def run(self, start, end=None, freq="6h", outpath=None, compute=False, write=True):
+        """
+        Run the preprocessing for a given date range.
+
+        Parameters
+        ----------
+        start : str or datetime-like
+            Start date.
+        end : str or datetime-like, optional
+            End date, by default None.
+        freq : str, optional
+            Frequency, by default "6h".
+        outpath : str, optional
+            Output path, by default None.
+        compute : bool, optional
+            Whether to compute the results, by default False.
+        write : bool, optional
+            Whether to write the results, by default True.
+
+        Returns
+        -------
+        list
+            List of preprocessed datasets or paths to the written files.
+        """
+        if outpath is None:
+            outpath = self.outpath
+        if end is None:
+            end = start
+        dates = datelist(start, end, freq=freq)
+        if outpath is not None:
+            outpath = outpath.format(date=dates[0])
+            ensure_dir(outpath)
+        print(f"writing to {outpath}")
+        result = [
+            self.preprocess(date, outpath=outpath if write is True else None)
+            for date in dates
+        ]
+        if compute:
+            result = dask.compute(*result)
+        if write is True and compute is True:
+            self._clean()
+        return result
+
+
+class CFPreprocessor(Preprocessor):
+    """
+    CFPreprocessor class for preparing input data from CF-compliant datasets.
+
+    Parameters
+    ----------
+    input_data : dict
+        Input data information.
+    *args : tuple
+        Additional arguments.
+    **kwargs : dict
+        Additional keyword arguments.
+    """
+
+    def __init__(
+        self,
+        expid,
+        surflib,
+        domain=None,
+        vc="vc_49lev",
+        outpath=None,
+        scratch=None,
+        input_data=None,
+        **kwargs,
+    ):
+        super().__init__(
+            expid, surflib, domain=domain, vc=vc, outpath=outpath, scratch=scratch
+        )
+        self.input_data = input_data
+        self.gfile = get_gcm_gfile(self.scratch.name, **self.input_data)
+        self.remap = remap
+
+    def get_input_dataset(self, date):
+        """
+        Get the input dataset for a given date.
+
+        Parameters
+        ----------
+        date : datetime-like
+            Date for the input dataset.
+
+        Returns
+        -------
+        xarray.Dataset
+            Input dataset.
+        """
+        return self.gfile.gfile(date).load()
+
+
+class ERA5Preprocessor(Preprocessor):
+    """
+    ERA5Preprocessor class for preparing input data from ERA5 datasets.
+
+    Parameters
+    ----------
+    input_data : dict
+        Input data information.
+    *args : tuple
+        Additional arguments.
+    **kwargs : dict
+        Additional keyword arguments.
+    """
+
+    def __init__(
+        self, expid, surflib, domain=None, vc="vc_49lev", outpath=None, scratch=None
+    ):
+        super().__init__(
+            expid, surflib, domain=domain, vc=vc, outpath=outpath, scratch=scratch
+        )
+
+    def get_input_dataset(self, date):
+        """
+        Get the input dataset for a given date.
+
+        Parameters
+        ----------
+        date : datetime-like
+            Date for the input dataset.
+
+        Returns
+        -------
+        xarray.Dataset
+            Input dataset.
+        """
+        filename = compute_gfile(date, self.scratch.name)
+        # logger.debug(f"created: {filename}")
+        ds = xr.open_dataset(filename).load()
+        return get_gcm_dataset(ds)
+
+
+class CloudPreprocessor(Preprocessor):
+
+    atm = ["ta", "ua", "va", "hus", "sftlf", "orog"]
+    ocn = ["tos"]
+
+    def __init__(
+        self,
+        expid,
+        surflib,
+        domain=None,
+        vc="vc_49lev",
+        outpath=None,
+        scratch=None,
+        input_data=None,
+        url=None,
+    ):
+        super().__init__(
+            expid, surflib, domain=domain, vc=vc, outpath=outpath, scratch=scratch
+        )
+        self.input_data = input_data
+        if url is None:
+            url = "gc"
+        if url in cloud_urls.keys():
+            url = cloud_urls[url]
+        self.url = url
+
+        intake_default_kwargs = {
+            "experiment_id": "historical",
+            "member_id": "r1i1p1f1",
+            "activity_id": "CMIP",
+            #  "table_id": ["6hrLev", "fx", "Oday"],
+            # "variable_id": self.atm + self.ocn,
+        }
+        intake_kwargs = intake_default_kwargs | input_data
+        cat = get_pangeo_catalog(self.url, **intake_kwargs)
+        dsets = get_dsets(cat.df[cat.df.variable_id.isin(self.atm)])
+        gcm = xr.merge(merge_dsets(dsets), join="override")
+
+        tos = open_zstore(cat.df[cat.df.variable_id == "tos"].iloc[0].zstore)
+        tos = tos.convert_calendar(tos.time.dt.calendar, use_cftime=True)
+        self.tos = tos
+        self.gcm = gcm
+        self.gfile = get_gcm_dataset(gcm.sel(time="1950-01"), tos=tos)
+
+    def get_input_dataset(self, date):
+        """
+        Get the input dataset for a given date.
+
+        Parameters
+        ----------
+        date : datetime-like
+            Date for the input dataset.
+
+        Returns
+        -------
+        xarray.Dataset
+            Input dataset.
+        """
+        return self.gfile.sel(time=date).load()
